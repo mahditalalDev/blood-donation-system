0d9541653e98e4ea0b93fcd5ab766495d94c76e8
mahdi <71930380@students.liu.edu.lb>
2024-05-12 13:10:28 +0300

    updated

--- Medical/js/index.js
+++ Medical/js/index.js
@@ -2,44 +2,34 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
 import {
   getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  createUserWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyB5piM8HyYATgWqMPi2U6bwAVz94Q189Bs",
-  authDomain: "fir-basics-569a0.firebaseapp.com",
-  projectId: "fir-basics-569a0",
-  storageBucket: "fir-basics-569a0.appspot.com",
-  messagingSenderId: "971203436246",
-  appId: "1:971203436246:web:11d5aa9c6a02ee8dc6f377",
-  measurementId: "G-LGY93EHL4E",
+  apiKey: "AIzaSyDfrHufsSDuxH1u2DsKx_3H6pWvgdXDcQk",
+  authDomain: "blooddoanationsystem.firebaseapp.com",
+  projectId: "blooddoanationsystem",
+  storageBucket: "blooddoanationsystem.appspot.com",
+  messagingSenderId: "1062989139800",
+  appId: "1:1062989139800:web:749dc6ad37e88970b45f2d",
+  measurementId: "G-9QXN765YNB"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const googleProvider = new GoogleAuthProvider();
+
 import {
   getFirestore,
   doc,
   getDoc,
   getDocs, //get all documents inside one collection
-  setDoc,
   collection,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  deleteField,
   query,
   where,
-  orderBy,
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 const db = getFirestore();
 const auth = getAuth();
 let usersCount = 0;
 let donationCount = 0;
-const bloodtypes = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
-let test = [];
+
 // --------------------------------
 // Reference to the "user" collection
 const userCollectionRef = collection(db, "users");
